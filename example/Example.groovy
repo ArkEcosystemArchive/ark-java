f5e5bc898be7f48688e266cc904abe65cefb1b3a
fix <fx.thoorens@gmail.com>
2017-04-11 11:00:16 +0200

    added example

--- example/Example.groovy
+++ example/Example.groovy
@@ -1,15 +1,3 @@
-# ark-java
-:coffee: Lite client library in Java
-
-
-# Authors
-FX Thoorens fx@ark.io
-
-
-# Example
-
-using groovy, see example/Example.groovy:
-```
 @GrabResolver(name='ark-java', root='https://dl.bintray.com/arkecosystem/ark-java/')
 @Grapes([
   @Grab('org.bitcoinj:bitcoinj-core:0.14.4'),
@@ -63,19 +51,3 @@ http.request(POST, JSON) {
 
   }
 }
-```
-
-then run:
-`groovy example/Example.groovy`
-
-# License
-
-The MIT License (MIT)
-
-Copyright (c) 2017 Ark
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
