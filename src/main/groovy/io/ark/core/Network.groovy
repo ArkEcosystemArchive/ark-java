155d810d1cbbd64b354ad6aa41029485e4ff95f9
pmd <per.dahlberg@gmail.com>
2017-12-29 09:58:17 +0100

    Code format.

--- src/main/groovy/io/ark/core/Network.groovy
+++ src/main/groovy/io/ark/core/Network.groovy
@@ -13,47 +13,47 @@ class Network extends Object {
 
   static Random random = new Random()
   static Network Mainnet = new Network(
-    nethash: '6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988',
-    prefix: 0x17,
-    port: 4001,
-    name: 'mainnet',
+      nethash: '6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988',
+      prefix: 0x17,
+      port: 4001,
+      name: 'mainnet',
       peerseed: [
-      "5.39.9.240:4001",
-      "5.39.9.241:4001",
-      "5.39.9.242:4001",
-      "5.39.9.243:4001",
-      "5.39.9.244:4001",
-      "5.39.9.250:4001",
-      "5.39.9.251:4001",
-      "5.39.9.252:4001",
-      "5.39.9.253:4001",
-      "5.39.9.254:4001",
-      "5.39.9.255:4001",
-      "5.39.53.48:4001",
-      "5.39.53.49:4001",
-      "5.39.53.50:4001",
-      "5.39.53.51:4001",
-    ]
+          "5.39.9.240:4001",
+          "5.39.9.241:4001",
+          "5.39.9.242:4001",
+          "5.39.9.243:4001",
+          "5.39.9.244:4001",
+          "5.39.9.250:4001",
+          "5.39.9.251:4001",
+          "5.39.9.252:4001",
+          "5.39.9.253:4001",
+          "5.39.9.254:4001",
+          "5.39.9.255:4001",
+          "5.39.53.48:4001",
+          "5.39.53.49:4001",
+          "5.39.53.50:4001",
+          "5.39.53.51:4001",
+      ]
   )
 
   static Network Devnet = new Network(
-    nethash: '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23',
-    prefix: 0x1e,
-    port: 4002,
-    name: 'devnet',
-    peerseed: [
-      "167.114.29.52:4002",
-      "167.114.29.53:4002",
-      "167.114.29.54:4002",
-      "167.114.29.55:4002"
-    ])
+      nethash: '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23',
+      prefix: 0x1e,
+      port: 4002,
+      name: 'devnet',
+      peerseed: [
+          "167.114.29.52:4002",
+          "167.114.29.53:4002",
+          "167.114.29.54:4002",
+          "167.114.29.55:4002"
+      ])
 
-  public Map getHeaders(){
-    [nethash:nethash, version:version, port:port]
+  public Map getHeaders() {
+    [nethash: nethash, version: version, port: port]
   }
 
-  public boolean warmup(){
-    if(peers.size()>0) return false
+  public boolean warmup() {
+    if (peers.size() > 0) return false
     peerseed.each {
       peers << Peer.create(it, this.headers)
     }
@@ -61,14 +61,14 @@ class Network extends Object {
   }
 
   // broadcast to many nodes
-  public int leftShift(Transaction transaction){
+  public int leftShift(Transaction transaction) {
     [1..broadcastMax].each {
       getRandomPeer() << transaction
     }
     return broadcastMax
   }
 
-  public Peer getRandomPeer(){
+  public Peer getRandomPeer() {
     peers[random.nextInt(peers.size())]
   }
 }
